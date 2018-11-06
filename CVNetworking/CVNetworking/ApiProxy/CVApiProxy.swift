0c23c92ad7a99c7e9eec4f4e35166c3eee8b9d5a
魏晓贺 <xiaohe.wei@fumubang.com>
2018-11-06 18:02:04 +0800

    --

---
+++ CVNetworking/CVNetworking/ApiProxy/CVApiProxy.swift
@@ -0,0 +1,25 @@
+//
+//  CVApiProxy.swift
+//  CVNetworking
+//
+//  Created by caven on 2018/11/6.
+//  Copyright © 2018 com.caven. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+/// 本类负责进行网络请求
+class CVApiProxy {
+    static let share = CVApiProxy()
+}
+
+// MARK: - 公有方法
+extension CVApiProxy {
+    
+}
+
+// MARK: - 私有方法
+private extension CVApiProxy {
+    
+}
